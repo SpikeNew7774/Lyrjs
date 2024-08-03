a06322d93b9a2f5cdd2879c53d339bea550f7c51
SpikeNew7774 <88393630+SpikeNew7774@users.noreply.github.com>
2024-08-03 20:31:07 +0200

    Temporary Downstatus - Removed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors")
 const { generateToken } = require("./addons/generateToken.js")
 
-//app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
